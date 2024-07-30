bb4c28c81578d136482e8cd9d487909728b8301c
Cristian <cristianvillalobos666@gmail.com>
2024-07-30 17:22:55 -0400

    dwasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,12 @@ import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
 import vercel from "@astrojs/vercel/serverless";
 
+const env = loadEnv("", process.cwd(), ["STORYBLOK", "NETLIFY"]);
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter:vercel(), // vercel() or netlify()
+  adapter: env.NETLIFY ? netlify() : vercel(), // vercel() or netlify()
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_PREVIEW_TOKEN,
